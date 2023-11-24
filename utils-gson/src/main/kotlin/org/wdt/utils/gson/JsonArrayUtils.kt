7370aa32c7abde11e34fef160e30cfdbefea39e6
Wdt~ <yuwenshuxue1@outlook.com>
2023-11-24 20:19:58 +0800

    utils-gson为了编译后的代码样式,采用内联方法

--- utils-gson/src/main/kotlin/org/wdt/utils/gson/JsonArrayUtils.kt
+++ utils-gson/src/main/kotlin/org/wdt/utils/gson/JsonArrayUtils.kt
@@ -1,33 +1,35 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package org.wdt.utils.gson
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 
-fun JsonArray.getString(index: Int): String {
+inline fun JsonArray.getString(index: Int): String {
 	return this.get(index).asString
 }
 
-fun JsonArray.getInt(index: Int): Int {
+inline fun JsonArray.getInt(index: Int): Int {
 	return this.get(index).asInt
 }
 
-fun JsonArray.getDouble(index: Int): Double {
+inline fun JsonArray.getDouble(index: Int): Double {
 	return this.get(index).asDouble
 }
 
-fun JsonArray.getBoolean(index: Int): Boolean {
+inline fun JsonArray.getBoolean(index: Int): Boolean {
 	return this.get(index).asBoolean
 }
 
-fun JsonArray.getLong(index: Int): Long {
+inline fun JsonArray.getLong(index: Int): Long {
 	return this.get(index).asLong
 }
 
-fun JsonArray.getJsonObject(index: Int): JsonObject {
+inline fun JsonArray.getJsonObject(index: Int): JsonObject {
 	return this.get(index).asJsonObject;
 }
 
-fun JsonArray.getJsonArray(index: Int): JsonArray {
+inline fun JsonArray.getJsonArray(index: Int): JsonArray {
 	return this.get(index).asJsonArray;
 }
 
--- utils-gson/src/main/kotlin/org/wdt/utils/gson/JsonObjectUtils.kt
+++ utils-gson/src/main/kotlin/org/wdt/utils/gson/JsonObjectUtils.kt
@@ -1,29 +1,31 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package org.wdt.utils.gson
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 
-fun JsonObject.getString(key: String): String {
+inline fun JsonObject.getString(key: String): String {
     return this.get(key).asString
 }
 
-fun JsonObject.getInt(key: String): Int {
+inline fun JsonObject.getInt(key: String): Int {
     return this.get(key).asInt
 }
 
-fun JsonObject.getBoolean(key: String): Boolean {
+inline fun JsonObject.getBoolean(key: String): Boolean {
     return this.get(key).asBoolean
 }
 
-fun JsonObject.getDouble(key: String): Double {
+inline fun JsonObject.getDouble(key: String): Double {
     return this.get(key).asDouble
 }
 
-fun JsonObject.getJsonObject(key: String): JsonObject {
+inline fun JsonObject.getJsonObject(key: String): JsonObject {
     return this.get(key).asJsonObject
 }
 
-fun JsonObject.getJsonArray(key: String): JsonArray {
+inline fun JsonObject.getJsonArray(key: String): JsonArray {
     return this.get(key).asJsonArray
 }
 
